4487149e3438f13359218baa248c6d5972c81b30
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-07-27 09:49:30 -0300

    cache
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.8.1
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 )
--- index.go
+++ index.go
@@ -2,16 +2,33 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/goreleaser/func/count"
+	"github.com/patrickmn/go-cache"
 )
 
+const key = "count"
+
+var c = cache.New(30*time.Minute, 40*time.Minute)
+
 func H(w http.ResponseWriter, r *http.Request) {
-	c, err := count.Count(r.Context())
+	cached, found := c.Get(key)
+	if found {
+		log.Println("from cache")
+		fmt.Fprint(w, cached)
+		return
+	}
+
+	live, err := count.Count(r.Context())
 	if err != nil {
+		log.Println("failed", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprint(w, c)
+	fmt.Fprint(w, live)
+	log.Println("live")
+	c.Set(key, live, cache.DefaultExpiration)
 }
